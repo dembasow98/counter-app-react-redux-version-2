2d03fb3ecf35cac6c9ce98acbb9b77261228f61d
dembasow98 <dastech1998@gmail.com>
2023-02-12 21:46:25 +0300

    Setup tailwindcss.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col w-full h-screen dark:bg-gray-900 dark:text-white content-center">
+      <div className="flex w-full max-w-xl dark:bg-gray-800">
+        <p className="text-2xl font-bold">Counter</p>
+      </div>
     </div>
   );
 }
